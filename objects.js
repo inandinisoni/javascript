c73a7a2bb13dac2d607a146b6d54ed0802522664
Nandini Soni <soninandini214@gmail.com>
2025-07-30 21:48:22 +0530

    Store student data as object
    storing student data like name phone number and marks as object using javascript

---
+++ objects.js
@@ -0,0 +1,10 @@
+//program to store name phone number and marks of a student as object
+let student = {
+    name: "Nandini Soni",
+    phone: "877003859411",
+    marks: {
+        math: 95,
+        science: 90,
+        english: 85
+    }
+};
